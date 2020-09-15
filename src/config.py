2f4c52ecf9a155c697fcf5375961311b39620e52
N.T.WORKS <nagisc007@yahoo.co.jp>
2020-09-15 09:22:48 +0900

    Dev memo (#3)
    * Update storybuilder (v0.6.1)
    
    * [WIP] plot memo
    
    * [WIP]
    
    * Add chnage log

--- src/config.py
+++ src/config.py
@@ -7,8 +7,9 @@ Story Config
 ASSET = {
         "PERSONS": (
             # (tag / name / full / age (birth) / job / call / info)
-            ('taro', '太郎', '', 17,(1,1), 'male', '学生'),
+            ('souta', '颯太', '伊那村,颯太', 17,(1,1), 'male', '高校生', "me:僕"),
             ("kosuke", "航輔", "耶麻本,航輔", 20,(1,1), "male", "大学生（Vチューバー）", "me:オレ"),
+            ("nagisa", "渚紗", "瀬戸川,渚紗", 15,(1,1), "female", "中学生", "me:うち"),
             ),
         "STAGES": (
             # (tag / name / parent / (geometry) / info)
@@ -24,6 +25,7 @@ ASSET = {
             ),
         "WORDS": (
             # (tag / name / cate / info)
+            ("game", "ミークラフト"),
             ),
         "RUBIS": (
             # (origin / rubi / exclusions / always)
